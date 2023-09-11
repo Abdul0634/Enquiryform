70eebff30e302507a6db8674410bf1aaf3fc9db0
Abdul0634 <abdulrahman29703@gmail.com>
2023-09-11 11:58:51 +0530

    add enquiry form

---
+++ script.js
@@ -0,0 +1,10 @@
+let loginForm = document.getElementById("myForm");
+
+loginForm.addEventListener("submit", (e) => {
+    // e.preventDefault()
+    {
+        alert("This form has successfully submitted");
+        window.location.reload()
+    }
+
+});
